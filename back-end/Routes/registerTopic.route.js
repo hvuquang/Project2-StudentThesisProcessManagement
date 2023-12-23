c327e3400d643e123a812e3c317ac0f26077c76b
nguyenquochuy753 <92136604+nguyenquochuy753@users.noreply.github.com>
2023-12-24 00:01:28 +0700

    feat: api student requests change the topic , teacher confirm the change topic request from the student, faculty confirm the change topic request from the teacher

--- back-end/Routes/registerTopic.route.js
+++ back-end/Routes/registerTopic.route.js
@@ -4,5 +4,8 @@ const registerTopicController = require("../Controllers/registerTopic.controller
 router.post("/registerTopic/:ma_sv&:id_topic",registerTopicController.registerTopic);
 router.get('/getAllRegisterTopic',registerTopicController.getAllRegisterTopic);
 router.get('/searchRegisterTopic', registerTopicController.getRegisterTopic);
+router.put("/changeTopic/:ma_sv&:id_new_topic", registerTopicController.studentChangeTopic);
+router.put("/teacherConfirmChangeTopic/:ma_sv", registerTopicController.teacherConfirmChangeTopicRequest);
+router.put("/facultyConfirmChangeTopic/:ma_sv", registerTopicController.facultyConfirmChangeTopic);
 
 module.exports = router;
